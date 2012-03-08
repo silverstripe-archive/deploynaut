0bfc7bc17d653cb882624929c95008cf01898891
Sam Minnee <sam@silverstripe.com>
2012-03-08 13:00:34 +1300

    Exclude a few more files from update-deploymaster

--- update-deploymaster.sh
+++ update-deploymaster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz --exclude nbproject --exclude .git -e  "ssh -p2222" . deploynaut@deploymaster:/sites/deploynaut/www/
+rsync -avz --exclude nbproject --exclude .git --exclude builds --exclude assets --exclude .gitignore --exclude .modules -e  "ssh -p2222" . deploynaut@deploymaster:/sites/deploynaut/www/
