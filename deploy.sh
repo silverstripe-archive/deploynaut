7430d186eaa4f969f356622558aaee45f215de7a
Mateusz Uzdowski <mateusz@silverstripe.com>
2012-10-01 10:42:55 +1300

    Deprecate deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-# Shortcut to a capistrano deploy
 
-function usage(){
-	echo === Builds ===
-	ls -l ../builds/ | awk '{print $9}' |  sed 's/.tar.gz//'
-	echo 
-	echo Usage example: $0 testing aa-b123
-	echo 
-	exit 1
-}
+echo "Deprecated: not compatible with deploynaut projects. Use cap directly (e.g. cap -T <project> deploy -s build=<tarball>)"
 
-BUILD_FILE="../builds/$2.tar.gz"
+exit 1
 
-if [ ! -f "$BUILD_FILE" ]; then
-	usage
-fi
-
-if [ -z $1 ]; then
-	usage
-fi
-
-cap $1 deploy -s build=$2
