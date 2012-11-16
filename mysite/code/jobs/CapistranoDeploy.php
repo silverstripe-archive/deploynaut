5da610c86b4e4bb7f77a8d93545199ab5c238839
Stig Lindqvist <stig@silverstripe.com>
2012-11-16 16:40:03 +1300

    Minor modification for redirection the capistrano output to file

--- mysite/code/jobs/CapistranoDeploy.php
+++ mysite/code/jobs/CapistranoDeploy.php
@@ -9,23 +9,21 @@ class CapistranoDeploy {
 	public $args;
 	
 	public function perform() {
-		
 		$environment = $this->args['environment'];
 		$buildname = $this->args['buildname'];
 		$logfile = $this->args['logfile'];
-		chdir(BASE_PATH);
-		$deploymentCommand = 'cap -v '.$environment.' deploy -s build='.$buildname;
 		$logfilePath = ASSETS_PATH.DIRECTORY_SEPARATOR.$logfile;
-		$command = '';
-		$command .= $deploymentCommand;
-		$command .= $command. " &> '{$logfilePath}'";
+		chdir(BASE_PATH);
+		$logfilePath = ASSETS_PATH.DIRECTORY_SEPARATOR.$logfile;
+		$deploymentCommand = 'cap -v '.$environment.' deploy -s build='.$buildname;
+		$command = $deploymentCommand. " > '{$logfilePath}' 2> '{$logfilePath}'";
 		echo '[=] Start deploy for "'.$environment.'" build "'.$buildname.'"'.PHP_EOL;
 		system($command, $status);
 		if($status===0) {
-			echo '[+] Success '.$logfile.PHP_EOL;
+				echo '[+] Success '.$logfile.PHP_EOL;
 		} else {
-			echo '[-] Fail '.$logfile.PHP_EOL;
-			throw new Exception('Deployment for "'.$environment.'" build "'.$buildname.'", logfile '.$logfile);
+				echo '[-] Fail '.$logfile.PHP_EOL;
+				throw new Exception('Deployment for "'.$environment.'" build "'.$buildname.'", logfile '.$logfile);
 		}
 	}
 }
