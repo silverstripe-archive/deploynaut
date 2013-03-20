95e2a1ff0d4aa2d18be322018165ddcd01e48a13
Stig Lindqvist <stig@stojg.net>
2013-03-20 19:32:52 +1300

    BUG: TEMP_FOLDER is different in CLI or WWW environment
    We have to force the setting of these deploynaut log and vcs paths in the _ss_environment file

--- mysite/_config.php
+++ mysite/_config.php
@@ -16,8 +16,21 @@ BasicAuth::protect_entire_site(true, "DEPLOYNAUT_ACCESS");
 
 if(defined('IS_SSL') && IS_SSL) Director::forceSSL();
 
-if(!defined('DEPLOYNAUT_LOG_PATH')) define('DEPLOYNAUT_LOG_PATH', TEMP_FOLDER . '/_deploynaut_logs');
-if(!defined('DEPLOYNAUT_LOCAL_VCS_PATH')) define('DEPLOYNAUT_LOCAL_VCS_PATH', TEMP_FOLDER . '/_local_repos');
-
-if(!file_exists(DEPLOYNAUT_LOG_PATH)) mkdir(DEPLOYNAUT_LOG_PATH);
-if(!file_exists(DEPLOYNAUT_LOCAL_VCS_PATH)) mkdir(DEPLOYNAUT_LOCAL_VCS_PATH);
+if(!defined('DEPLOYNAUT_LOG_PATH')) {
+	// The reason is that sys_get_temp_dir() can be different in cli and webserver environment.
+	throw new RuntimeException('You must set the DEPLOYNAUT_LOG_PATH in _ss_environment.php');
+}
+if(!defined('DEPLOYNAUT_LOCAL_VCS_PATH')) {
+	// The reason is that sys_get_temp_dir() can be different in cli and webserver environment.
+	throw new RuntimeException('You must set the DEPLOYNAUT_LOCAL_VCS_PATH in _ss_environment.php');
+}
+if(!file_exists(DEPLOYNAUT_LOG_PATH)) {
+	mkdir(DEPLOYNAUT_LOG_PATH);
+	// Make sure the cli and www have write access to this folder
+	chmod(DEPLOYNAUT_LOG_PATH, 0777);
+}
+if(!file_exists(DEPLOYNAUT_LOCAL_VCS_PATH)) {
+	mkdir(DEPLOYNAUT_LOCAL_VCS_PATH);
+	// Make sure the cli and www have write access to this folder
+	chmod(DEPLOYNAUT_LOCAL_VCS_PATH, 0777);
+}
