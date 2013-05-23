152c4a8526a89ccf2138bd7f13d33d1a652df777
Sean Harvey <sean@silverstripe.com>
2013-05-23 14:24:58 +1200

    log all output from resque workers into a log file for debugging

--- mysite/code/jobs/CapistranoDeploy.php
+++ mysite/code/jobs/CapistranoDeploy.php
@@ -16,34 +16,43 @@ class CapistranoDeploy {
 		chdir(BASE_PATH);
 	}
 
+	protected function getLogHandle() {
+		$logfile = DEPLOYNAUT_LOG_PATH . '/' . $this->args['logfile'];
+		$fh = fopen($logfile, 'a');
+		if(!$fh) {
+			throw new RuntimeException('Can\'t open file "'.$logfile.'" for logging.');
+		}
+		return $fh;
+	}
+
 	public function perform() {
 		$environment = $this->args['environment'];
 		$repository = $this->args['repository'];
 		$sha = $this->args['sha'];
-		$logfile = DEPLOYNAUT_LOG_PATH . '/' . $this->args['logfile'];
 		$projectName = $this->args['projectName'];
 
 		$project = DNProject::get()->filter('Name', $projectName)->first();
 		GraphiteDeploymentNotifier::notify_start($environment, $sha, null, $project);
-		echo '[-] Deploying "'.$sha.'" to "'.$projectName.':'.$environment.'"'.PHP_EOL;
 
-		$command = $this->getCommand($projectName.':'.$environment, $repository, $sha, $logfile);
-		$command ->run(function ($type, $buffer) use($logfile) {
-			$fh = fopen($logfile, 'a');
-			if(!$fh) {
-				throw new RuntimeException('Can\'t open file "'.$logfile.'" for logging.');
-			}
+		$fh = $this->getLogHandle();
+		fwrite($fh, 'Deploying "'.$sha.'" to "'.$projectName.':'.$environment.'"'.PHP_EOL);
+
+		$command = $this->getCommand($projectName.':'.$environment, $repository, $sha);
+		$command->run(function ($type, $buffer) use($fh) {
 			do {
 				usleep(rand(1, 10000));
 			} while (!flock($fh, LOCK_EX));
 			fwrite($fh, $buffer);
 			flock($fh, LOCK_UN);
-			fclose($fh);
 		});
 		if(!$command->isSuccessful()) {
 			throw new RuntimeException($command->getErrorOutput());
 		}
-		echo '[-] Deploy done "'.$sha.'" to "'.$projectName.':'.$environment.'"'.PHP_EOL;
+
+		fwrite($fh, 'Deploy done "'.$sha.'" to "'.$projectName.':'.$environment.'"'.PHP_EOL);
+
+		fclose($fh);
+
 		GraphiteDeploymentNotifier::notify_end($environment, $sha, null, $project);
 	}
 
@@ -55,13 +64,14 @@ class CapistranoDeploy {
 	 * @param string $logfile
 	 * @return \Symfony\Component\Process\Process
 	 */
-	protected function getCommand($environment, $repository, $sha, $logfile) {
+	protected function getCommand($environment, $repository, $sha) {
 		$command = 'cap -vv '.$environment.' deploy';
 		$command.= ' -s repository='.$repository;
 		$command.= ' -s branch='.$sha;
 		$command.= ' -s history_path='.realpath(DEPLOYNAUT_LOG_PATH.'/');
 
-		echo "[-] Running command: $command";
+		$fh = $this->getLogHandle();
+		fwrite($fh, "Running command: $command");
 
 		$process = new \Symfony\Component\Process\Process($command);
 		$process->setTimeout(3600);
--- mysite/code/jobs/CloneGitRepo.php
+++ mysite/code/jobs/CloneGitRepo.php
@@ -8,26 +8,33 @@ class CloneGitRepo {
 		set_time_limit(0);
 		$path = $this->args['path'];
 		$repo = $this->args['repo'];
-		
+		$logfile = DEPLOYNAUT_LOG_PATH . '/clonegitrepo.log';
+		$fh = fopen($logfile, 'a');
+		if(!$fh) {
+			throw new RuntimeException('Can\'t open file "'.$logfile.'" for logging.');
+		}
+
 		if(file_exists($path)) {
 			$this->delTree($path);
 		}
 		mkdir($path, 0777, true);
-		echo '[+] Cloning ' . $repo.' to ' . $path . PHP_EOL;
+
+		fwrite($fh, 'Cloning ' . $repo.' to ' . $path . PHP_EOL);
 
 		// using git clone straight via system call since doing it via the
 		// Gitonomy\Git\Admin times out. Silly.
 		$command = sprintf('git clone --bare -q %s %s', $repo, $path);
+
+		fwrite($fh, 'Running command: ' . $command . PHP_EOL);
+
 		exec($command, $output, $return_var);
 
 		if($return_var != 0) {
-			$file = DEPLOYNAUT_LOG_PATH . '/clonegitrepo.log';
-			$fh = fopen($file, 'a');
 			fwrite($fh, sprintf('\'%s\' exited with return code %s. Output: "%s"' . PHP_EOL, $command, $return_var, var_export($output, true)));
 			fclose($fh);
 		}
 
-		echo '[+] Cloned ' . $repo . ' to ' . $path . PHP_EOL;
+		echo fwrite($fh, 'Cloned ' . $repo . ' to ' . $path . PHP_EOL);
 	}
 
 	protected function delTree($dir) {
