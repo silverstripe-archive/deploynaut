f09f53f81eca278d1b2ee000bd39e3b6b60e7ffb
Sam Minnee <sam@silverstripe.com>
2013-07-01 13:36:23 +1200

    FIX: Fixed hard-coding of localhost into new metric dashboard.

--- mysite/code/model/GraphiteGraph.php
+++ mysite/code/model/GraphiteGraph.php
@@ -9,6 +9,7 @@ class GraphiteGraph extends ViewableData {
 
 	function Graph($width = null, $height = null) {
 		$this->graphite->setTime('1h');
+		$this->graphite->setBaseURL(Director::absoluteURL('graphite/'));
 		return $this->graphite->getDashboardHTML($width, $height);
 	}
 }
