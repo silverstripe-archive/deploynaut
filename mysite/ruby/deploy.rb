017bc0677beae9689ebf7b5cb1322b324482f635
Mateusz Uzdowski <mateusz@silverstripe.com>
2013-07-19 15:03:56 +1200

    NEW Add ability to hook pre and post migration scripts.

--- mysite/ruby/deploy.rb
+++ mysite/ruby/deploy.rb
@@ -10,6 +10,11 @@ _cset(:sake_path) { "./framework/sake" }
 namespace :deploy do
 
 	task :migrate do
+		# Run custom pre-migration script.
+		if exists?(:pre_migrate_script)
+			run "if [ -f \"#{pre_migrate_script}\" ]; then #{pre_migrate_script} && echo \"Pre-migrate script returned $?\"; fi"
+		end
+
 		if exists?(:webserver_user)
 			run "sudo -u #{webserver_user} bash #{latest_release}/#{sake_path} dev/build flush=1", :roles => :db
 		else
@@ -21,6 +26,11 @@ namespace :deploy do
 		# Initialise the cache, in case dev/build wasn't executed on all hosts
 		if exists?(:webserver_user)
 			run "sudo -u #{webserver_user} bash #{latest_release}/#{sake_path} dev"
+		end
+
+		# Run custom post-migration script.
+		if exists?(:post_migrate_script)
+			run "if [ -f \"#{post_migrate_script}\" ]; then #{post_migrate_script} && echo \"Post-migrate script returned $?\"; fi"
 		end
 	end
 
