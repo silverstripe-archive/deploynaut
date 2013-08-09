ff1e619560aa5d031aef3a1833712447e13a8e6b
Sam Minnee <sam@silverstripe.com>
2013-08-09 16:23:39 +1200

    Further updates to clean up module

--- _config.php
+++ _config.php
@@ -1,17 +1,1 @@
 <?php
-
-global $project;
-$project = 'mysite';
-
-global $database;
-$database = 'deploynaut';
-
-require_once('conf/ConfigureFromEnv.php');
-
-MySQLDatabase::set_connection_charset('utf8');
-
-SSViewer::set_theme('deploynaut');
-
-BasicAuth::protect_entire_site(true, "DEPLOYNAUT_ACCESS");
-
-if(defined('IS_SSL') && IS_SSL) Director::forceSSL();
