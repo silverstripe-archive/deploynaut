93d18cef3a9995d0db1833f601d885d3c35bdc9b
Stig Lindqvist <stig@silverstripe.com>
2013-11-19 14:53:18 +1300

    Move the deploynaut modeladmin up to the top of the cms menu

--- code/control/DNAdmin.php
+++ code/control/DNAdmin.php
@@ -2,8 +2,28 @@
 
 class DNAdmin extends ModelAdmin {
 
+	/**
+	 *
+	 * @var string 
+	 */
 	public static $menu_title = "Deploynaut Projects";
+	
+	/**
+	 *
+	 * @var string 
+	 */
 	public static $url_segment = "naut";
+	
+	/**
+	 *
+	 * @var array 
+	 */
 	public static $managed_models = array('DNProject');
+	
+	/**
+	 *
+	 * @var string 
+	 */
+	private static $menu_priority = 100;
 
 }
