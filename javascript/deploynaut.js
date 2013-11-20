29e24b659638d8e3dbdf379432ec005efa835952
Stig Lindqvist <stig@silverstripe.com>
2013-11-20 16:40:50 +1300

    $.post in javascript don't work properly in some environments

--- javascript/deploynaut.js
+++ javascript/deploynaut.js
@@ -2,27 +2,25 @@
 /*global jQuery: false */
 (function($) {
 	"use strict";
-	
+
 	var deploy = {
-		showlog: function ($status, $content, logLink) {
+		showlog: function($status, $content, logLink) {
 			var self = this;
-			$.getJSON(logLink, { randval: Math.random()},
-				function(data){
-					$status.text(data.status);
-					$content.text(data.content);
-					$status.attr('class', data.status);
-					$('title').text('Deployment is ' + data.status);
-					if(data.status === 'Complete') {
-						self._clearInterval();
-					}
+			$.getJSON(logLink, {randval: Math.random()},
+			function(data) {
+				$status.text(data.status);
+				$content.text(data.content);
+				$status.attr('class', data.status);
+				$('title').text('Deployment is ' + data.status);
+				if (data.status === 'Complete') {
+					self._clearInterval();
 				}
+			}
 			);
 		},
-		
 		start: function() {
 			this._setupPinging();
 		},
-		
 		/**
 		 * Will fetch latest deploy log and reload the content with it
 		 */
@@ -32,7 +30,6 @@
 				self.showlog($("#deploy_action"), $("#deploy_log"), $('#deploy_log').data('loglink'));
 			}, 3000);
 		},
-
 		/**
 		 * Will remove the pinging and refresh of the application list
 		 */
@@ -41,63 +38,73 @@
 		}
 	};
 
-	$(document).ready(function(){
-		
-		if($('#Form_DeployForm_BuildName').val() === '') {
+	$(document).ready(function() {
+
+		if ($('#Form_DeployForm_BuildName').val() === '') {
 			$('#Form_DeployForm_action_doDeploy').attr('disabled', true);
 		}
-		
-		$('#Form_DeployForm_BuildName').change(function(){
-			if($('#Form_DeployForm_BuildName').val() === '') {
+
+		$('#Form_DeployForm_BuildName').change(function() {
+			if ($('#Form_DeployForm_BuildName').val() === '') {
 				$('#Form_DeployForm_action_doDeploy').attr('disabled', true);
 				return;
 			}
 			$('#Form_DeployForm_action_doDeploy').attr('disabled', false);
 		});
-		
+
 		$('#Form_DeployForm_action_doDeploy').click(function() {
 			return confirm('Are you sure that you want to deploy?');
 		});
-		
+
 		// Deployment screen
-		if($('#deploy_log').length) {
+		if ($('#deploy_log').length) {
 			deploy.start();
 		}
 
 		$('.project-branch > h3').click(function() {
 			var $project = $(this).parent();
-			if($project.hasClass('open')) {
+			if ($project.hasClass('open')) {
 				$project.removeClass('open');
 			} else {
 				$project.addClass('open');
 			}
 		});
 
-		$('a.update-repository').click(function(e){
+		$('a.update-repository').click(function(e) {
 			e.preventDefault();
 
 			$(this).attr('disabled', 'disabled');
 			$(this).html('Fetching');
 			$(this).toggleClass('loading');
-			
-			$.post($(this).data('api-url')+'.json', function(data){
-				$('#gitFetchModal').modal('show');
-				$('#gitFetchModal').on('hidden', function () {
-					location.reload();
-				});
-			
-				window.fetchInterval = window.setInterval(function() {
-					$.get(data.location+'.json', function(log_data){
-						$('#gitFetchModal .modal-body').html('<pre>'+log_data.message+'</pre>');
-						$('#gitFetchModal .modal-footer').html('Status: '+log_data.status);
-						if(log_data.status === 'Failed' || log_data.status === 'Success' ) {
-							clearInterval(window.fetchInterval);
-						}
+
+			$.ajax({
+				type: "POST",
+				url: $(this).data('api-url'),
+				dataType: 'json',
+				success: function(data) {
+					$('#gitFetchModal').modal('show');
+					$('#gitFetchModal').on('hidden', function() {
+						location.reload();
 					});
-				}, 2000);
+
+					window.fetchInterval = window.setInterval(function() {
+						$.ajax({
+							type: "GET",
+							url: data.location,
+							dataType: 'json',
+							success: function(log_data) {
+								$('#gitFetchModal .modal-body').html('<pre>' + log_data.message + '</pre>');
+								$('#gitFetchModal .modal-footer').html('Status: ' + log_data.status);
+								if (log_data.status === 'Failed' || log_data.status === 'Success') {
+									clearInterval(window.fetchInterval);
+								}
+							}
+						});
+					}, 2000);
+				}
 			});
 		});
-		
+
 		$('.tooltip-hint').tooltip({
 			placement: "top",
 			trigger: 'hover'
