ce86f188316fd593b100b411175ea3f1d757402b
Sean Harvey <sean@silverstripe.com>
2014-01-23 11:19:46 +1300

    Finishing off getdb so that it actually works
    data_path defines where the SQL dump should end up on the deploynaut
    server, passed into cap like so: "cap -s data_path=/tmp"

--- ruby/data.rb
+++ ruby/data.rb
@@ -1,39 +1,76 @@
 namespace :data do
+
+	desc <<-DESC
+		Get a database off the target server, given the contents of the _ss_environment.php file.
+		Output the file contents to a path on the deploynaut server
+		Assumption: This task will run against the webserver, so that it's able to fetch the database credentials
+		from _ss_environment.php
+
+		Required arguments to the cap command:
+		data_path - Output the SQL dump to this path on the deploynaut server
+
+		Example command: cap -f '/sites/deploynaut/www/assets/Capfile' project1:env1 data:getdb -s data_path=/tmp
+	DESC
 	task :getdb do
-		puts mysql_options
-		# todo
+		database_name = getdatabasename
+
+		# todo: output to gzip and stream that back instead of the raw data.
+		run "mysqldump --add-drop-table #{mysql_options} -p" do |channel, stream, data|
+			if data =~ /^Enter password: /
+				channel.send_data "#{getmysqlpassword}\n"
+			end
+
+			begin
+				file = File.open(data_path + "/#{database_name}-" + Time.now.to_i.to_s + ".sql", "a")
+				file.write(data)
+			rescue IOError => e
+				# error writing the file.
+			ensure
+				file.close unless file == nil
+			end
+		end
 	end
 
 	task :getassets do
 		# todo
 	end
 
-	# return a string with mysql options, e.g. "-u user -p12345 -h hostname SS_mysite"
-	def mysql_options
-		database_username = ""
-		database_password = ""
+	def getdatabasename
 		database_name = ""
-		database_host = ""
-
-		# todo: "do |_, _, database_username" doesn't work here, is that possible?
-		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_USERNAME;" } do |channel, stream, data|
-			database_username = data
-		end
-
-		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_PASSWORD;" } do |channel, stream, data|
-			database_password = data
-		end
-
 		# todo: if SS_DATABASE_NAME isn't defined, how do we get the database name?
 		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_NAME;" } do |channel, stream, data|
 			database_name = data
 		end
 
-		# return the target machine hostname, strip newlines
-		run "echo `hostname`" do |channel, stream, data|
-			database_host = data.strip
+		database_name
+	end
+
+	def getmysqlpassword
+		database_password = ""
+
+		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_PASSWORD;" } do |channel, stream, data|
+			database_password = data
 		end
 
-		"-u #{database_username} -p#{database_password} -h #{database_host} #{database_name}"
+		database_password
+	end
+
+	# return a string with mysql options, e.g. "-u user -h hostname SS_mysite"
+	# but without the password, for security reasons.
+	def mysql_options
+		database_server = ""
+		database_username = ""
+		database_host = ""
+
+		# todo: "do |_, _, database_username" doesn't work here, is that possible?
+		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_SERVER;" } do |channel, stream, data|
+			database_server = data
+		end
+
+		run %Q{ php -r "require_once '#{current_path}/../../_ss_environment.php'; echo SS_DATABASE_USERNAME;" } do |channel, stream, data|
+			database_username = data
+		end
+
+		"-u #{database_username} -h #{database_server} #{getdatabasename}"
 	end
 end
