9a0f69f4b4dfa1dae1851b9e7b888d3f7d3c31e3
Sean Harvey <sean@silverstripe.com>
2014-02-04 13:38:45 +1300

    If an error-503.html file exists in a site, use that for maint page

--- ruby/maintenance.rb
+++ ruby/maintenance.rb
@@ -4,7 +4,6 @@ namespace :maintenance do
 		Copies a template .htaccess and html page into the current release directory.
 
 		TODO: Hardcoded deploynaut path /sites/deploynaut/www
-		TODO: Use existing assets/error-503.html in website if available
 		TODO: Only supports Apache. Doesn't work with nginx... yet.
 	DESC
 	task :enable do
@@ -12,7 +11,20 @@ namespace :maintenance do
 
 		run "mv #{current_path}/.htaccess #{current_path}/.htaccess_original; true"
 
-		upload("/sites/deploynaut/www/deploynaut/maintenance.html.template", current_path + "/maintenance.html")
+		# if there's an error-503.html file available in assets, use that for the maintenance page
+		custom_maintenance = nil
+		custom_maintenance_path = "#{shared_path}/assets/error-503.html"
+		run "[ -f \"#{custom_maintenance_path}\" ]; then cp #{custom_maintenance_path} #{current_path}/maintenance.html && echo 1; else echo 0; fi" do |_, _, data|
+			if data[0] == "1"
+				custom_maintenance = true
+			end
+		end
+
+		# if there's no custom maintenance page found above, use a default one supplied with deploynaut
+		if custom_maintenance == nil
+			upload("/sites/deploynaut/www/deploynaut/maintenance.html.template", current_path + "/maintenance.html")
+		end
+
 		upload("/sites/deploynaut/www/deploynaut/maintenance.htaccess.template", current_path + "/.htaccess")
 	end
 
