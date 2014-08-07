silverstripe-archive/deploynaut

commit cc86470e7d2ae2c4c6b651fa8380e89550ba8d92

Support for running "git clone" as another user using sudo.

code/jobs/CloneGitRepo.php
@@ -35,7 +35,14 @@ class CloneGitRepo {
 
 		// using git clone straight via system call since doing it via the
 		// Gitonomy\Git\Admin times out. Silly.
-		$command = sprintf('git clone --bare -q %s %s', $repo, $path);
+
+		// if an alternate user has been configured for clone, run the command as that user
+		$user = Injector::inst()->get('DNData')->getGitUser();
+		if($user) {
+			$command = sprintf('sudo -u %s git clone --bare -q %s %s', $user, $repo, $path);
+		} else {
+			$command = sprintf('git clone --bare -q %s %s', $repo, $path);
+		}
 
 		fwrite($fh, '['.date('Y-m-d H:i:s').'] Running command: ' . $command . PHP_EOL);
 

code/jobs/FetchJob.php
@@ -26,9 +26,14 @@ class FetchJob {
 		
 		$log->write('Starting git fetch for project "' . $project->Name . '"');
 
-		$repository = new Gitonomy\Git\Repository($path, array(
-			'environment_variables' => $env
-		));
+		$options = array('environment_variables' => $env);
+		// if an alternate user has been configured for clone, run the command as that user
+		$user = Injector::inst()->get('DNData')->getGitUser();
+		if($user) {
+			$options['command'] = sprintf('sudo -u %s git', $user);
+		}
+
+		$repository = new Gitonomy\Git\Repository($path, $options);
 		$repository->run('fetch', array('-p', 'origin', '+refs/heads/*:refs/heads/*', '--tags'));
 		$log->write('Git fetch is finished');
 	}

code/model/DNData.php
@@ -29,16 +29,27 @@ class DNData {
 	protected $projectList;
 
 	/**
+	 * If set, this provides an alternate user to the current one
+	 * executing the Git shell commands. e.g. if this is set to
+	 * a user called "composer", any Git commands that Deploynaut
+	 * will perform will be run as "sudo -u composer git ..."
+	 *
+	 * @var string|null
+	 */
+	protected $gitUser = null;
+
+	/**
 	 *
 	 * @var DeploymentBackend
 	 */
 	protected $backend;
 
-	public function __construct($environmentDir, $keyDir, $dataTransferDir) {
+	public function __construct($environmentDir, $keyDir, $dataTransferDir, $gitUser = null) {
 		$this->backend = Injector::inst()->get('DeploymentBackend');
 		$this->setEnvironmentDir($environmentDir);
 		$this->setKeyDir($keyDir);
 		$this->setDataTransferDir($dataTransferDir);
+		$this->setGitUser($gitUser);
 	}
 
 	public function getEnvironmentDir() {
@@ -57,6 +68,14 @@ class DNData {
 	public function setKeyDir($keyDir) {
 		if($keyDir[0] != "/") $keyDir = BASE_PATH . '/' . $keyDir;
 		$this->keyDir = $keyDir;
+	}
+
+	public function getGitUser() {
+		return $this->gitUser;
+	}
+
+	public function setGitUser($user) {
+		$this->gitUser = $user;
 	}
 
 	public function getDataTransferDir() {

commit 73aee30a39c2a95816d989f05ae7a7856f8ef683

Fixes for running Git commands under an alternate user in deploynaut
Fixes a regression in cc86470e7d2ae2c4c6b651fa8380e89550ba8d92 where
Gitonomy "command" parameter doesn't accept any use of "sudo -u user".
Use Symfony process directly in FetchJob cases.

If another user is cloning a repo, CloneGitRepo will re-create the
local git directory it under the www-data user, which isn't appropriate.
It should all be handled by the other user in this case

git clone already runs mkdir() to create a directory, so mkdir()
shouldn't be needed in CloneGitRepo.

In order for the other user to remove an existing local repo when
CloneGitRepo is called, it needs to be run under that user. To do this,
remove the use of delTree and just "rm -rf" the path directly.

code/jobs/CloneGitRepo.php
@@ -25,10 +25,16 @@ class CloneGitRepo {
 			throw new RuntimeException('Can\'t open file "'.$logfile.'" for logging.');
 		}
 
+		// if an alternate user has been configured for clone, run the command as that user
+		$user = Injector::inst()->get('DNData')->getGitUser();
+
 		if(file_exists($path)) {
-			$this->delTree($path);
+			if($user) {
+				exec(sprintf('sudo -u %s rm -rf %s', $user, $path));
+			} else {
+				exec(sprintf('rm -rf %s', $path));
+			}
 		}
-		mkdir($path, 0777, true);
 
 		fwrite($fh, '['.date('Y-m-d H:i:s').'] Cloning ' . $repo.' to ' . $path . PHP_EOL);
 		echo "[-] CloneGitRepo starting" . PHP_EOL;
@@ -36,8 +42,6 @@ class CloneGitRepo {
 		// using git clone straight via system call since doing it via the
 		// Gitonomy\Git\Admin times out. Silly.
 
-		// if an alternate user has been configured for clone, run the command as that user
-		$user = Injector::inst()->get('DNData')->getGitUser();
 		if($user) {
 			$command = sprintf('sudo -u %s git clone --bare -q %s %s', $user, $repo, $path);
 		} else {
@@ -57,11 +61,4 @@ class CloneGitRepo {
 		fwrite($fh, '['.date('Y-m-d H:i:s').'] Cloned ' . $repo . ' to ' . $path . PHP_EOL);
 	}
 
-	protected function delTree($dir) {
-		$files = array_diff(scandir($dir), array('.','..'));
-		foreach ($files as $file) {
-			(is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file");
-		}
-		return rmdir($dir);
-	}
 }

code/jobs/FetchJob.php
@@ -26,15 +26,25 @@ class FetchJob {
 		
 		$log->write('Starting git fetch for project "' . $project->Name . '"');
 
-		$options = array('environment_variables' => $env);
 		// if an alternate user has been configured for clone, run the command as that user
+		// @todo Gitonomy doesn't seem to have any way to prefix the command properly, if you
+		// set 'sudo -u composer git' as the "command" parameter, it tries to run the whole
+		// thing as a single command and fails
 		$user = Injector::inst()->get('DNData')->getGitUser();
 		if($user) {
-			$options['command'] = sprintf('sudo -u %s git', $user);
+			$command = sprintf('cd %s && sudo -u %s git fetch -p origin +refs/heads/*:refs/heads/* --tags', $path, $user);
+			$process = new \Symfony\Component\Process\Process($command);
+			$process->setEnv($env);
+			$process->setTimeout(3600);
+			$process->run();
+			if(!$process->isSuccessful()) {
+				throw new RuntimeException($process->getErrorOutput());
+			}
+		} else {
+			$repository = new Gitonomy\Git\Repository($path, array('environment_variables' => $env));
+			$repository->run('fetch', array('-p', 'origin', '+refs/heads/*:refs/heads/*', '--tags'));
 		}
 
-		$repository = new Gitonomy\Git\Repository($path, $options);
-		$repository->run('fetch', array('-p', 'origin', '+refs/heads/*:refs/heads/*', '--tags'));
 		$log->write('Git fetch is finished');
 	}
 }