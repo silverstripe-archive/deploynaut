b951a18bf3f3f14e9c992c3298d10c668628ed29
Paul Clarke <paul@silverstripe.com>
2015-08-07 11:21:23 +1200

    responsive tabs added to deploy tabs

--- javascript/deploynaut.js
+++ javascript/deploynaut.js
@@ -14,6 +14,71 @@
       e.preventDefault();
     });
 
+    // Scrolling tabs
+    var hidWidth;
+	var scrollBarWidths = 40;
+
+	var widthOfList = function(){
+	  var itemsWidth = 0;
+	  $('.list li').each(function(){
+	    var itemWidth = $(this).outerWidth();
+	    itemsWidth+=itemWidth;
+	  });
+	  return itemsWidth;
+	};
+
+	var widthOfHidden = function(){
+	  return (($('.wrapper').outerWidth())-widthOfList()-getLeftPosi())-scrollBarWidths;
+	};
+
+	var getLeftPosi = function(){
+	  return $('.list').position().left;
+	};
+
+	var reAdjust = function(){
+	  if (($('.wrapper').outerWidth()) < widthOfList()) {
+	    $('.scroller-right').show();
+	  }
+	  else {
+	    $('.scroller-right').hide();
+	  }
+	  
+	  if (getLeftPosi()<0) {
+	    $('.scroller-left').show();
+	  }
+	  else {
+	    $('.item').animate({left:"-="+getLeftPosi()+"px"},'slow');
+	  	$('.scroller-left').hide();
+	  }
+	}
+
+	reAdjust();
+
+	$(window).on('resize',function(e){  
+	  	reAdjust();
+	});
+
+	$('.scroller-right').click(function() {
+	  
+	  $('.scroller-left').fadeIn('slow');
+	  $('.scroller-right').fadeOut('slow');
+	  
+	  $('.list').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){
+
+	  });
+	});
+
+	$('.scroller-left').click(function() {
+	  
+		$('.scroller-right').fadeIn('slow');
+		$('.scroller-left').fadeOut('slow');
+	  
+	  	$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){
+	  	
+	  	});
+	});    
+	//Scrolling tabs end
+
 
 	var queue = {
 		showlog: function(status, content, logLink) {
