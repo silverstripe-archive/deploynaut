eab07ae275817f1aba42b8468d5c88d834af11b2
Mateusz Uzdowski <mateusz@silverstripe.com>
2015-09-29 09:35:48 +1300

    BUG Properly check for holder's existence.

--- js/base.jsx
+++ js/base.jsx
@@ -1,9 +1,10 @@
 var DeploymentDialog = require('./deployment_dialog.jsx');
 
 // Mount the component only on the page where the holder is actually present.
-if (typeof document.getElementById('deployment-dialog-holder')!=='undefined') {
+var holder = document.getElementById('deployment-dialog-holder');
+if (holder) {
 	React.render(
 		<DeploymentDialog context = {environmentConfigContext} />,
-		document.getElementById('deployment-dialog-holder')
+		holder
 	);
 }
