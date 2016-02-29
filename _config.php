bf25a759aede7dc88847271b0f35f7530f232f31
Michael Strong <mstrong@silverstripe.com>
2016-02-29 01:08:43 +0000

    Improved detection on failed snapshots to prevent stale 'running' jobs.

--- _config.php
+++ _config.php
@@ -6,3 +6,10 @@ Deprecation::notification_version('1.1.0', 'deploynaut');
 $exts = Config::inst()->get('File', 'allowed_extensions');
 $exts[] = 'sspak';
 Config::inst()->update('File', 'allowed_extensions', $exts);
+
+Resque_Event::listen('onFailure', function(Exception $exception, Resque_job $job) {
+	$inst = $job->getInstance();
+	if($inst instanceof DeploynautJobInterface) {
+		$inst->onFailure($exception);
+	}
+});
