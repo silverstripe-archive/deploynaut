b1d864b2dd98eeb8b1402f492143d1f5b6132155
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-09-08 15:13:32 +1200

    capture api errors in a better way (#635)
    As an example I have added error handling to the DeployHistory and CurrentBuildStatus components

--- js/reducers/deployhistory.js
+++ js/reducers/deployhistory.js
@@ -9,18 +9,21 @@ module.exports = function deployhistory(state, action) {
 			page_length: 0,
 			total_pages: 0,
 			current_page: 0,
-			is_loading: false
+			is_loading: false,
+			error: null
 		};
 	}
 
 	switch (action.type) {
 		case actions.START_DEPLOY_HISTORY_GET:
 			return _.assign({}, state, {
-				is_loading: true
+				is_loading: true,
+				error: null
 			});
 		case actions.FAIL_DEPLOY_HISTORY_GET:
 			return _.assign({}, state, {
-				is_loading: false
+				is_loading: false,
+				error: action.error.toString()
 			});
 		case actions.SUCCEED_DEPLOY_HISTORY_GET:
 			return _.assign({}, state, {
