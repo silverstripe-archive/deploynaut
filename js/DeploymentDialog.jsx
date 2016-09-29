silverstripe-archive/deploynaut

commit 4dab8cc8ea65c9039f121278c359a2023bce1d3d

First version of the styling of the deployment modal

js/DeploymentDialog.jsx
@@ -135,7 +135,6 @@ var DeploymentDialog = React.createClass({
 				<div className={classes} onClick={this.handleClick}>
 					<span className="status-icon" aria-hidden="true"></span>
 					<span className="time">last updated {this.state.last_fetched}</span>
-					<EnvironmentName environmentName={this.props.context.envName} />
 				</div>
 				{form}
 			</div>

code/control/DNRoot.php
@@ -174,6 +174,7 @@ class DNRoot extends Controller implements PermissionProvider, TemplateGlobalPro
 
 		Requirements::javascript('deploynaut/javascript/bootstrap.file-input.js');
 		Requirements::javascript('deploynaut/thirdparty/select2/dist/js/select2.min.js');
+		Requirements::javascript('deploynaut/javascript/selectize.js');
 		Requirements::javascript('deploynaut/thirdparty/bootstrap-switch/dist/js/bootstrap-switch.min.js');
 		Requirements::javascript('deploynaut/javascript/material.js');
 

js/Modal.jsx
@@ -47,7 +47,7 @@ var Modal = React.createClass({
 		}
 
 		return (
-			<div className={classNames} tabIndex="-1" ref={function(node) { this.selector = node; }.bind(this)}>
+			<div className={classNames} ref={function(node) { this.selector = node; }.bind(this)}>
 				<div className="modal-dialog modal-lg">
 					<div className="modal-content">
 						<div className="modal-header">

js/components/Dropdown.jsx
@@ -30,43 +30,42 @@ var Dropdown = React.createClass({
 	},
 
 	componentDidMount: function() {
-		this.destroySelect2();
-		this.initSelect2();
-	},
-
-	componentDidUpdate: function() {
-		this.destroySelect2();
-		this.initSelect2();
+		this.initSelectize();
 	},
 
 	componentWillUnmount: function() {
-		this.destroySelect2();
+		// we want to destroy selectize to prevent
+		// onUpdate events to hang around after this
+		// component have been unmounted.
+		this.removeSelectize();
 	},
 
 	selector: null,
 
-	initSelect2: function() {
-		$(this.selector).select2(this.props.options);
-		$(this.selector).select2().on("change", function(evt) {
-			const value = evt.currentTarget.value;
+	// We are using the selectize library instead of select2 because
+	// select2 causes a lot of bugs and the hacks didn't do it anymore.
+	// The biggest problem is displaying a select2 dropdown in a bootstrap
+	// modal that has overflow scrolling will break the scrolling.
+	initSelectize: function() {
+		$(this.selector).selectize(this.props.options);
+		// push the set the default value from react to selectize
+		if (typeof this.props.value !== 'undefined') {
+			$(this.selector)[0].selectize.setValue(this.props.value);
+		}
+		// make sure that react get notified when values are picked
+		$(this.selector)[0].selectize.on("change", function(value) {
 			this.props.onSelect(value);
 		}.bind(this));
-
-		if (typeof this.props.value !== 'undefined') {
-			$(this.selector).val(this.props.value);
-		}
 	},
 
-	destroySelect2: function() {
-		if ($(this.selector).data('select2')) {
-			$(this.selector).select2().off("change");
-			$(this.selector).select2("destroy");
+	removeSelectize: function() {
+		if ($(this.selector)[0].selectize) {
+			$(this.selector)[0].selectize.destroy();
 		}
 	},
 
 	render: function() {
 		var props = this.props;
-
 		var options = [];
 		var idx = 0;
 		if (props.options) {
@@ -80,17 +79,15 @@ var Dropdown = React.createClass({
 						{props.options[index].value}
 					</option>
 				);
-
 			});
 		}
 
 		options.unshift(<option key={"default"} value={""} disabled>{props.defaultText}</option>);
-		var className = 'form-control';
 		return (
 			<select
 				ref={function(node) { this.selector = node; }.bind(this)}
 				id={props.name}
-				className={className}
+				className='form-control disable-select2'
 				name={props.name}
 				onChange={props.onSelect}
 				value={props.value}

js/components/StepMenu.jsx
@@ -4,20 +4,16 @@ var React = require("react");
 // with the proper design
 
 var Step = function(props) {
-	var classNames = "";
-	var style = {
-		marginBottom: "4px"
-	};
+	let classNames = "step";
 
 	if (props.active) {
-		classNames = "alert-info";
-	} else if (!props.show) {
-		style.color = "#ddd";
+		classNames += " active";
+	} else if (props.show) {
+		classNames += " selectable";
 	}
 
 	var onclick = function() {};
 	if (props.show) {
-		style.cursor = "pointer";
 		onclick = props.onClick;
 	}
 
@@ -33,9 +29,8 @@ var Step = function(props) {
 
 	return (
 		<div
-			style={style}
 			onClick={onclick}
-			className={"alert " + classNames}
+			className={classNames}
 		>
 			{props.id + 1}. {props.title} {loading} {check}
 		</div>

js/components/TextArea.jsx
@@ -3,6 +3,7 @@ var React = require("react");
 var TextArea = function(props) {
 	return (
 		<textarea
+			className="form-control"
 			rows="10"
 			name={props.name}
 			value={props.value}

js/containers/Approval.jsx
@@ -19,7 +19,10 @@ function Approval(props) {
 	}
 
 	return (
-		<div>
+
+		<div className="section">
+			<header id="2">Approval</header>
+
 			<p>
 				Once this request has been approved, your team will be able to
 				progress to the final Deployment step.

commit 330f5f16887391689107b8f96b88987293362369

Fixing regression in 4dab8cc

js/DeploymentDialog.jsx
@@ -135,6 +135,7 @@ var DeploymentDialog = React.createClass({
 				<div className={classes} onClick={this.handleClick}>
 					<span className="status-icon" aria-hidden="true"></span>
 					<span className="time">last updated {this.state.last_fetched}</span>
+					<EnvironmentName environmentName={this.props.context.envName} />
 				</div>
 				{form}
 			</div>