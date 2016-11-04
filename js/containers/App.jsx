768c7284d21e4b8f746e8b13a46c3561bc3abfa4
Sean Harvey <sean@silverstripe.com>
2016-11-04 13:48:08 +1300

    Removing placeholder text for now

--- js/containers/App.jsx
+++ js/containers/App.jsx
@@ -11,9 +11,6 @@ function App(props) {
 			<CurrentBuildStatus />
 			<div className="row">
 				<div className="col-md-9">
-					<p>New deployments can be taken through an approval process to ensure the correct
-					code is being released. For a detailed description of the <a href="#">permissions</a> and <a href="#">deployment process</a> please
-					refer to the <a href="#">deployment documentation</a>.</p>
 				</div>
 				<div className="col-md-3 text-right">
 					<ReactRouter.Link className="btn btn-primary deploy-button" to="/deployment/new">
