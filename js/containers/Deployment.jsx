d113a3da0f4f6480395bea16026ad605174d313f
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-11-08 11:50:46 +1300

    Load logs inside the deploy section component (#769)

--- js/containers/Deployment.jsx
+++ js/containers/Deployment.jsx
@@ -1,52 +1,67 @@
 const React = require("react");
 const ReactRedux = require('react-redux');
+const actions = require('../_actions.js');
 
 const Deploy = require('./buttons/Deploy.jsx');
 
-const deployment = function(props) {
-
-	function shouldShowLogs() {
-		if (typeof props.deploy_log === 'undefined') {
-			return false;
-		}
-		return props.deploy_log.length > 0;
+function shouldShowLogs(props) {
+	if (typeof props.deploy_log === 'undefined') {
+		return false;
 	}
+	return props.deploy_log.length > 0;
+}
 
-	let error = null;
-	if (props.error) {
-		error = (
-			<div className="alert alert-danger">
-				<div className="">
-					{props.error}
+const deployment = React.createClass({
+
+	componentDidMount: function() {
+		this.logUpdateInterval = setInterval(() => {
+			this.props.updateLogs();
+		}, 5 * 1000);
+	},
+
+	componentWillUnmount: function() {
+		clearInterval(this.logUpdateInterval);
+	},
+
+	logUpdateInterval: null,
+
+	render: function() {
+		let error = null;
+		if (this.props.error) {
+			error = (
+				<div className="alert alert-danger">
+					<div className="">
+						{this.props.error}
+					</div>
+				</div>
+			);
+		}
+
+		let logOutput = null;
+		if (shouldShowLogs(this.props)) {
+			const lines = Object.keys(this.props.deploy_log).map(function(key) {
+				return <div key={key}>{this.props.deploy_log[key]}</div>;
+			}.bind(this));
+			logOutput = (
+				<pre>
+				{lines}
+			</pre>
+			);
+		}
+
+		return (
+			<div className="section deployment">
+				{error}
+				<div className="deploy-btn-container">
+					<Deploy sha={this.props.selected_ref} />
+				</div>
+				<div>
+					{logOutput}
 				</div>
 			</div>
 		);
 	}
-
-	let logOutput = null;
-	if (shouldShowLogs()) {
-		const lines = Object.keys(props.deploy_log).map(function(key) {
-			return <div key={key}>{props.deploy_log[key]}</div>;
-		});
-		logOutput = (
-			<pre>
-				{lines}
-			</pre>
-		);
-	}
-
-	return (
-		<div className="section deployment">
-			{error}
-			<div className="deploy-btn-container">
-				<Deploy sha={props.selected_ref} />
-			</div>
-			<div>
-				{logOutput}
-			</div>
-		</div>
-	);
-};
+});
 
 const mapStateToProps = function(state) {
 	return {
@@ -56,8 +71,12 @@ const mapStateToProps = function(state) {
 	};
 };
 
-const mapDispatchToProps = function() {
-	return {};
+const mapDispatchToProps = function(dispatch) {
+	return {
+		updateLogs: function() {
+			dispatch(actions.getDeployLog());
+		}
+	};
 };
 
 module.exports = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(deployment);
