c97b35791030122cb150b5fb3061014e0fe84a01
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-11-09 12:07:21 +1300

    bug: completing step 2 causes step 1 to show loading bar
    After finishing the step 2. Deployment plan by clicking the
    [continue] button, the first panel is showing a loading bar

--- js/containers/buttons/SaveDeployPlan.jsx
+++ js/containers/buttons/SaveDeployPlan.jsx
@@ -6,7 +6,7 @@ var Button = require('../../components/Button.jsx');
 const mapStateToProps = function(state) {
 	return {
 		display: state.deployment.id === "",
-		disabled: state.plan.is_loading || state.deployment.is_loading,
+		disabled: state.plan.is_loading || state.deployment.is_creating,
 		style: "btn-success",
 		value: "Continue"
 	};
