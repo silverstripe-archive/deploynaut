1b13a2f83d793e1041a380daaf6af09e486f59fe
Sean Harvey <sean@silverstripe.com>
2016-11-11 15:45:40 +1300

    Ensure user permissions are checked before showing actions

--- js/containers/buttons/RejectRequest.jsx
+++ js/containers/buttons/RejectRequest.jsx
@@ -1,13 +1,26 @@
-var ReactRedux = require('react-redux');
+const ReactRedux = require('react-redux');
 
-var actions = require('../../_actions.js');
-var Button = require('../../components/Button.jsx');
+const actions = require('../../_actions.js');
+const Button = require('../../components/Button.jsx');
 const constants = require('../../constants/deployment.js');
 
-const mapStateToProps = function(state) {
+function canReject(state) {
+	if (!state.user.can_approve) {
+		return false;
+	}
 	const current = state.deployment.list[state.deployment.current_id] || {};
+	if (constants.isApproved(current.state)) {
+		return false;
+	}
+	if (constants.isRejected(current.state)) {
+		return false;
+	}
+	return true;
+}
+
+const mapStateToProps = function(state) {
 	return {
-		display: constants.isSubmitted(current.state),
+		display: canReject(state),
 		disabled: state.deployment.is_loading,
 		style: "btn-wide btn-danger",
 		value: "Reject"
