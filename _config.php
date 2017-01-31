be79ed07b907ddf81dbc78a3e2a0feac87e53532
Sean Harvey <sean@silverstripe.com>
2017-02-01 09:20:14 +1300

    Add common configuration that both SSP and CWP projects duplicate.

--- _config.php
+++ _config.php
@@ -1,7 +1,5 @@
 <?php
 
-Deprecation::notification_version('1.1.0', 'deploynaut');
-
 // *.sspak is required for data archives
 $exts = Config::inst()->get('File', 'allowed_extensions');
 $exts[] = 'sspak';
@@ -14,3 +12,35 @@ Resque_Event::listen('onFailure', function(Exception $exception, Resque_job $job
 		$inst->onFailure($exception);
 	}
 });
+
+// set default from address in emails, unless otherwise overriden by specific emails
+if(defined('DEPLOYNAUT_ADMIN_EMAIL')) {
+	Config::inst()->update('Email', 'admin_email', DEPLOYNAUT_ADMIN_EMAIL);
+}
+
+// send fatal errors and warnings to the defined error email
+if(defined('DEPLOYNAUT_ERROR_EMAIL')) {
+	SS_Log::add_writer(new SS_LogEmailWriter(DEPLOYNAUT_ERROR_EMAIL), SS_Log::WARN, '<=');
+}
+
+if (!defined('DEPLOYNAUT_LOG_PATH')) {
+	throw new RuntimeException('You must set the DEPLOYNAUT_LOG_PATH in _ss_environment.php');
+}
+if (!defined('DEPLOYNAUT_LOCAL_VCS_PATH')) {
+	throw new RuntimeException('You must set the DEPLOYNAUT_LOCAL_VCS_PATH in _ss_environment.php');
+}
+
+if (!defined('DEPLOYNAUT_SSH_KEY')) {
+	throw new RuntimeException('You must set the DEPLOYNAUT_SSH_KEY in _ss_environment.php. It should point to a private keyfile');
+}
+
+if (!file_exists(DEPLOYNAUT_LOG_PATH)) {
+	mkdir(DEPLOYNAUT_LOG_PATH, 0777, true);
+	// Make sure the cli and www have write access to this folder
+	chmod(DEPLOYNAUT_LOG_PATH, 0777);
+}
+if (!file_exists(DEPLOYNAUT_LOCAL_VCS_PATH)) {
+	mkdir(DEPLOYNAUT_LOCAL_VCS_PATH, 0777, true);
+	// Make sure the cli and www have write access to this folder
+	chmod(DEPLOYNAUT_LOCAL_VCS_PATH, 0777);
+}
